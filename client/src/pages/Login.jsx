d41410bf6d7209f1b73dfd63288d90ebef87c149
Aswabnth.v <aswanthv695@gmail.com>
2025-02-06 07:46:53 -0800

    completed login section

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,6 +3,9 @@ import {useForm} from 'react-hook-form'
 import {Link} from 'react-router-dom'
 import {useDispatch } from 'react-redux'
 import {TbDog } from 'react-icons/tb'
+import {BsShare} from "react-icons/bs"
+import {AiOutlineInteraction} from 'react-icons/ai'
+import {ImConnection} from 'react-icons/im'
 import { TextInput,Loading,CustomButton } from '../components'
 import { BgImage,NoProfile } from '../assets'
 
@@ -13,6 +16,10 @@ function Login() {
   }=useForm({
     mode:"onChange"
   })
+
+  const onSubmit = async(data)=>{
+    
+  }
 
   const [errMsg ,setErrMsg]=useState('');
   const [isSubmitting, setIsSubmitting]=useState(false)
@@ -26,16 +33,18 @@ function Login() {
         <div className='w-full lg:w-2/3 h-full p-10 2xl:px-20 flex flex-col justify-center'>
         
         <div className='w-full flex gap-2 item-center mb-6'>
-        <div className='p-2 bg-[#065ad8] rounded text-white'>
+        <div className='p-2 bg-[#967BB6] rounded text-white'>
         <TbDog size={30 } />
         </div>
-        <span className='text-2xl text-[#065ad8]' font-semibold>
+        <span className='text-2xl text-[#B57EDC]' font-semibold>
           Sharefun
         </span>
         </div>
         <p className='text-ascent-1 text-base front-semibold'>Log in to your account</p>
         <span className='text-sm mt-2 text-ascent-2'>Welcome Back</span>
-        <form className='py-8 flex flex-col gap-5'>
+        <form className='py-8 flex flex-col gap-5'
+        onSubmit={handleSubmit(onSubmit)}
+        >
           <TextInput
           name='email' 
           placeholder='email@example.com'
@@ -96,7 +105,7 @@ function Login() {
         >Don't have an account?{''}
         <Link
         to='/register'
-        className='text-[#065ad8] font-semibold ml-2 cursor-pointer'>
+        className='text-[#B57EDC] font-semibold ml-2 cursor-pointer'>
         Create Account</Link>
         </p>
         </div>
@@ -110,8 +119,30 @@ function Login() {
           alt='Bg Image'
           className='w-48 2xl:w-64 h-48 2xl:h-64 rounded-full object-cover'
           />
+          <div className="absolute flex items-center gap-1 bg-white right-3 top-10 py-1.5 px-3 rounded-full">
+
+            <BsShare size={10}/>
+            <span className='text-xs font-medium'>Share</span>
+          </div>
+
+          <div className="absolute flex items-center gap-1 bg-white left-4 top-6 py-1.5 px-2 rounded-full ">
+            <ImConnection/>
+            <span className='text-xs font-medium'>Connect</span>
+          </div>
+
+          <div className="absolute flex items-center gap-1 bg-white left-3 bottom-6 py-1.5 px-2 rounded-full ">
+            <AiOutlineInteraction/>
+            <span className='text-xs font-medium'>Interact</span>
+          </div>
           </div>
           
+          <div className="mt-16 text-center" >
+
+            <p className="text-white text-base" >Connect & help be kind </p>
+            <span className="text-sm text-white/80">Share and find Helping hands</span>
+          </div>
+
+
         </div>
       </div>
     </div>
