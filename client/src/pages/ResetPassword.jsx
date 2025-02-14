a14ec9e5a1c3832b400128bb24a92701e11e387c
Aswabnth.v <aswanthv695@gmail.com>
2025-02-14 10:00:51 -0800

    completed Reset password structure

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { CustomButton, Loading, TextInput } from '../components';
 
-function ResetPassword() {
+const ResetPassword=()=> {
+    const [errMsg,setErrMsg]=useState("");
+    const [isSubmiting,setIsSubmiting] =useState(false)
+
+    const {
+      register,
+      handleSubmit,
+      getValues,
+      watch,
+      formState:{ errors },
+    } =useForm({
+      mode:'onChange'
+    })
 
 
-  c
+  const onSubmit =async(data)=>{};
+
   return (
-    <div>ResetPassword</div>
+    <div className='w-full h-[100vh] bg-bgColor flex items-center justify-center p-6'>
+        <div className="bg-primary w-full md:w-1/3 2xl:w-1/4 px-6 py-8 shadow-md rounded-lg">
+          <p className="text-ascent-1 text-lg font-semibold">Email Address</p>
+          <span className="text-sm text-ascent-2">Enter email address used during registration </span>
+          <form onSubmit={handleSubmit(onsubmit)} className='py-5 flex flex-col gap-5'>
+
+
+            <TextInput
+           name='email'
+           placeholder='email@example.com'
+           type='email'
+           register={register("email",{
+            required:"Email Address is required"
+           })} 
+           styles='w-full rounded-lg'
+           labelStyle='ml-2'
+           error={errors.email ? errors.email.message :""}    
+        />
+        {errMsg?.message &&(
+          <span role='alert'
+          className={`text-sm ${
+            errMsg?.status === "failed"
+            ? "text-[#f64949fe]"
+            :"text-[#2ba150fe]"
+          }`}>
+
+          </span>
+        )}
+  {isSubmiting ? (
+    <Loading/>
+  ) :(
+    <CustomButton
+    type='submit'
+    containerStyle={`inline-flex justify-center rounded-md bg-blue px-8 py-3 text-sm font-medium text-white outline-none`}
+    title='Submit'
+    
+    />
+  )
+
+  }
+
+          </form>
+        </div>
+    </div>
   )
 }
 
