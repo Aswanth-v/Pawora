848a659b9402c7b438b0b6c201318fb75d5f2987
Aswabnth.v <aswanthv695@gmail.com>
2025-02-21 08:52:14 -0800

    started CardProfile & added profile sec

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,7 @@ import TextInput from "./TextInput";
 import Loading from "./Loading";
 import CustomButton from "./CustomButton";
 import TopBar from "./TopBar";
+import CardProfile from "./CardProfile";
 export {
-    Loading,TextInput,CustomButton,TopBar
+    Loading,TextInput,CustomButton,TopBar,CardProfile
 }
