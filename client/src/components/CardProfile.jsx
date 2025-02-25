d54eea9d9f173c251ad4abaac582e5b6de6fad90
Aswabnth.v <aswanthv695@gmail.com>
2025-02-24 22:30:38 -0800

    cardprofile section is completed for now

--- client/src/components/CardProfile.jsx
+++ client/src/components/CardProfile.jsx
@@ -2,34 +2,104 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { NoProfile } from "../assets";
+import { LiaEditSolid } from "react-icons/lia";
+import moment from 'moment'
+import { BsBriefcase, BsFacebook, BsInstagram, BsPersonFillAdd } from "react-icons/bs";
+import {FaTwitterSquare} from 'react-icons/fa'
+import { CiLocationOn } from "react-icons/ci";
+import { UpdateProfile } from "../redux/userSlice";
 
 const ProfileCard = ({ user }) => {
-    const { user: data, edit } = useSelector((state) => state.user);
-    const dispatch = useDispatch();
-  
-    return (
-      <div>
-        <div className='w-full bg-primary flex flex-col items-center shadow-sm rounded-xl px-6 py-4 '>
-          <div className='w-full flex items-center justify-between border-b pb-5 border-[#66666645]'>
-            <Link to={"/profile/" + user?._id} className='flex gap-2'>
-              <img
-                src={user?.profileUrl ?? NoProfile}
-                alt={user?.email}
-                className='w-14 h-14 object-cover rounded-full'
-              />
+  const { user: data, edit } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-              <div className="flex flex-col justify-center">
-                  <p className="text-lg font-medium text-ascent-1">
+  return (
+    <div>
+      <div className="w-full bg-primary flex flex-col items-center shadow-sm rounded-xl px-6 py-4 ">
+        <div className="w-full flex items-center justify-between border-b pb-5 border-[#66666645]">
+          <Link to={"/profile/" + user?._id} className="flex gap-2">
+            <img
+              src={user?.profileUrl ?? NoProfile}
+              alt={user?.email}
+              className="w-14 h-14 object-cover rounded-full"
+            />
 
-                  {user?.firstName} {user?.lastName}
-                  </p>
-                  <span className="text-ascent-2">{user?.profession ??"No profession"}</span>
-              </div>
-                    </Link>
-                </div>
+            <div className="flex flex-col justify-center">
+              <p className="text-lg font-medium text-ascent-1">
+                {user?.firstName} {user?.lastName}
+              </p>
+              <span className="text-ascent-2">
+                {user?.profession ?? "No profession"}
+              </span>
             </div>
+          </Link>
+          <div className="">
+            {user?.id === data?.id ? (
+              <LiaEditSolid
+                side={22}
+                className="text-blue cursor-pointer"
+                onClick={() => dispatch(UpdateProfile(true))}
+              />
+            ) : (
+              <button
+                className="bg-[#0444a430] text-sm text-white p-1 rounded"
+                onClick={() => {}}
+              >
+                <BsPersonFillAdd size={20} className="text-white" />
+              </button>
+            )}
+          </div>
         </div>
-    );
+
+        <div className="w-full flex flex-col gap-2 py-4 border-b border-['#66666645']">
+          <div className="flex gap-2 items-center text-ascent-2">
+            <CiLocationOn className="text-xl text-ascent-1" />
+            <span>{user?.location ?? "Add Location"}</span>
+          </div>
+          <div className="flex gap-2 items-center text-ascent-2">
+            <BsBriefcase className="text-lg text-ascent-1" />
+            <span>{user.profession ?? "Add profession"}</span>
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col gap-2 py-4 border-b border[#66666645]">
+          <p className="text-xl text-ascent-1 font-semibold">
+            {user?.friends?.length} Friends
+          </p>
+
+          <div className="flex items-center justify-between">
+            <span className="text-ascent-2">Who viewed your profile</span>
+            <span className="text-ascent-1 text-md">{user?.views?.length}</span>
+          </div>
+        </div>
+        <span className="text-base text-blue">{user?.verified ? "Verified Account" : "Not Verified"}</span>
+
+        <div className="flex items-center justify-between">
+          <span className="text-ascent-2">Joined</span>
+          <span className="text-ascent-1 text-base">
+            {moment(user?.createdAt).fromNow()}
+          </span>
+        </div>
+        <div className="w-full flex flex-col gap-4 py-4 pb-6">
+          <p className="text-ascent-1 text-lg font-semibold">social Accounts</p>
+          <div className="flex gap-2 items-centre text-ascent-2">
+            <BsInstagram className="text-xl text-ascent-1"/>
+            <span>Instagram</span>
+          </div>
+        
+          <div className="flex gap-2 items-centre text-ascent-2">
+            <FaTwitterSquare className="text-xl text-ascent-1"/>
+            <span>Twitter</span>
+          </div>
+          <div className="flex gap-2 items-centre text-ascent-2">
+            <BsFacebook className="text-xl text-ascent-1"/>
+            <span>Facebook</span>
+          </div>
+        </div>
+        </div>
+      </div>
+   
+  );
 };
 
 export default ProfileCard; // Correct export
