5b109d61d03c638c68c94ef3b84f4b798b700400
Aswabnth.v <aswanthv695@gmail.com>
2025-05-11 02:21:41 -0700

    added error.html and success.html to show if emailverification success or not

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,29 @@
 import express from "express";
 import path from "path";
-import { verifyEmail} from "../controllers/userController.js";
-import { resetPasswordLink } from "../utils/sendEmail.js";
+import { requestPasswordReset, verifyEmail,resetPassword,changePassword} from "../controllers/userController.js";
+
 const router = express.Router();
 const __dirname = path.resolve(path.dirname(""));
 
 router.get("/verify/:userId/:token", verifyEmail);
-router.get("/reset-password/:userId/:token", resetPasswordLink);
+//password reaser
+router.post('/request-passwordreset',requestPasswordReset)
+router.get("/reset-password/:userId/:token", resetPassword);
+router.post('/reset-password',changePassword)
+
+
 
 router.get("/verified", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views", "index.html"));
+  const status = req.query.status;
+ 
+  
+  if (status === 'success') {
+    res.sendFile(path.join(__dirname, "./views", "success.html"));
+  } else if (status === 'error') {
+    res.status(404).sendFile(path.join(__dirname, "./views", "error.html"));
+  } else {
+    res.status(400).send("Invalid status parameter.");
+  }
 });
 
 
