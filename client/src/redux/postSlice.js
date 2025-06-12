d8e99e40705661bdd81699f06948e9f480b700ee
Aswabnth.v <aswanthv695@gmail.com>
2025-06-12 08:46:34 -0700

    forgot to create getPost un postSlice.js .now problem solved

--- client/src/redux/postSlice.js
+++ client/src/redux/postSlice.js
@@ -1,21 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  posts: {},
+};
 
-const initialState={
-    posts:{},
-}
-
-const postSlice=createSlice({
-    name:'post',
-    initialState,
-    reducers:{
-        getPost(state,action){
-            state.posts =action.payload
-        }
-    }
-})
+const postSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {
+    getPosts(state, action) {
+      state.posts = action.payload;
+    },
+  },
+});
 
 export default postSlice.reducer;
+
 export function SetPosts(post) {
   return (dispatch, getState) => {
     dispatch(postSlice.actions.getPosts(post));
