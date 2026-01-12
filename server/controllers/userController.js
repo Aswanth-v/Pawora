eddd6cef692d01ae0d594bd96af8de6b2a8f2608
Aswabnth.v <aswanthv695@gmail.com>
2026-01-12 08:21:25 -0800

    changed filename

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import Verification from "../models/emailVerification.js";
 import Users from "../models/userModel.js";
 import { compareString, createJWT, hashString } from "../utils/index.js";
-import passwordReset from "../models/PasswordReset.js";
+import passwordReset from "../models/passwordReset.js";
 import { resetPasswordLink } from "../utils/sendEmail.js";
 import dotenv from "dotenv";
 import FriendRequest from "../models/friendRequest.js";
